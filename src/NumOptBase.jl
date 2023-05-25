c0298ee9642628ae73e25cbd6478cf7ba9b29780
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-05-25 12:50:59 +0200

    First implementation of the API

--- src/NumOptBase.jl
+++ src/NumOptBase.jl
@@ -1,5 +1,339 @@
+"""
+
+Package `NumOptBase` provides basic operations on "variables" for numerical
+optimization methods.
+
+"""
 module NumOptBase
 
-# Write your package code here.
+using Unitless
 
+"""
+    @assert_same_indices A B ...
+
+throws a `DimensionMismatch` exception if arrays `A`, `B`, etc. do not have the
+same indices.
+
+"""
+macro assert_same_indices(syms::Symbol...)
+    esc(_assert_same_indices(syms))
 end
+
+function _assert_same_indices(syms::Union{Tuple{Vararg{Symbol}},AbstractVector{Symbol}})
+    if (n = length(syms)) < 2
+        return :nothing
+    else
+        buf = IOBuffer()
+        write(buf, "arrays")
+        ex = Expr(:comparison)
+        resize!(ex.args, 2*n - 1)
+        i = 0
+        for sym in syms
+            if i > 0
+                ex.args[2i] = :(==)
+            end
+            ex.args[2i+1] = :(Base.axes($sym))
+            i += 1
+            sep = i == 1 ? " `" : i < n ? ", `" : n == 2 ? " and `" : ", and `"
+            write(buf, sep, sym, '`')
+        end
+        write(buf, " must have the same indices")
+        return :($ex || Base.throw(Base.DimensionMismatch($(String(take!(buf))))))
+    end
+end
+
+"""
+    NumOptBase.apply!(dst, f, args...) -> dst
+
+overwrites destination `dst` with the result of applying the mapping `f` to
+arguments `args...`.
+
+This method shall be extended to specific argument types.
+
+"""
+apply!
+
+"""
+    NumOptBase.scale!(dst, α, x) -> dst
+
+overwrites destination `dst` with `α⋅x` and returns `dst`. If `iszero(α)`
+holds, zero-fill `dst` whatever the values in `x`.
+
+"""
+function scale!(dst::AbstractArray{T,N}, α::Real, x::AbstractArray{T,N}) where {T,N}
+    @assert_same_indices dst x
+    unsafe_scale!(dst, α, x)
+    return dst
+end
+
+function unsafe_scale!(dst::AbstractArray,
+                       α::Real,
+                       x::AbstractArray)
+    if iszero(α)
+        fill!(dst, zero(eltype(dst)))
+    elseif isone(α)
+        dst !== x && copyto!(dst, x)
+    elseif α == -one(α)
+        unsafe_map!(-, dst, x)
+    else
+        α = convert_multiplier(α, x)
+        unsafe_map!(xᵢ -> α*xᵢ, dst, x)
+    end
+    nothing
+end
+
+"""
+    NumOptBase.update!(x, β, y) -> x
+
+overwrites destination `x` with `x + β⋅y` and returns `x`. This is a shortcut
+for `NumOptBase.combine!(x,1,x,β,y)`.
+
+"""
+function update!(dst::AbstractArray{T,N},
+                 α::Real, x::AbstractArray{T,N}) where {T,N}
+    @assert_same_indices dst x
+    unsafe_update!(dst, α, x)
+    return dst
+end
+
+
+function unsafe_update!(dst::AbstractArray,
+                        α::Real, x::AbstractArray)
+    if isone(α)
+        unsafe_map!(+, dst, dst, x)
+    elseif α == -one(α)
+        unsafe_map!(-, dst, dst, x)
+    elseif !iszero(α)
+        α = convert_multiplier(α, x)
+        unsafe_map!((dstᵢ, xᵢ) -> dstᵢ + α*xᵢ, dst, dst, x)
+    end
+    nothing
+end
+
+"""
+    NumOptBase.multiply!(dst, x, y) -> dst
+
+overwrites destination `dst` with the element-wise multiplication (Hadamard
+product) of `x` by `y` and returns `dst`.
+
+"""
+function multiply!(dst::AbstractArray{T,N},
+                   x::AbstractArray{T,N},
+                   y::AbstractArray{T,N}) where {T,N}
+    @assert_same_indices dst x y
+    unsafe_map!(*, dst, x, y)
+    return dst
+end
+
+"""
+    NumOptBase.combine!(dst, α, x, β, y) -> dst
+
+overwrites destination `dst` with the linear combination `α⋅x + β⋅y` and
+returns `dst`.
+
+"""
+@inline function combine!(dst::AbstractArray{T,N},
+                          α::Real, x::AbstractArray{T,N},
+                          β::Real, y::AbstractArray{T,N}) where {T,N}
+    @assert_same_indices dst x y
+    unsafe_combine!(dst, α, x, β, y)
+    return dst
+end
+
+function unsafe_combine!(dst::AbstractArray,
+                         α::Real, x::AbstractArray,
+                         β::Real, y::AbstractArray)
+    if iszero(β)
+        unsafe_scale!(dst, α, x)
+    elseif iszero(α)
+        unsafe_scale!(dst, β, y)
+    elseif isone(α)
+        if isone(β)
+            #dst .= x .+ y
+            unsafe_map!(+, dst, x, y)
+        elseif β == -one(β)
+            #dst .= x .- y
+            unsafe_map!(-, dst, x, y)
+        else
+            # dst .= x .+ β.*y
+            β = convert_multiplier(β, y)
+            unsafe_map!((xᵢ, yᵢ) -> xᵢ + β*yᵢ, dst, x, y)
+        end
+    else
+        #dst .= α.*x .+ β.*y
+        α = convert_multiplier(α, x)
+        β = convert_multiplier(β, y)
+        unsafe_map!((xᵢ, yᵢ) -> α*xᵢ + β*yᵢ, dst, x, y)
+    end
+    nothing
+end
+
+"""
+    NumOptBase.convert_multiplier(α, x)
+
+converts scalar real `α` to a floating-point type whose numerical precision is
+the same as that of the elements of `x`.
+
+"""
+convert_multiplier(α::Real, x::AbstractArray) =
+    convert(floating_point_type(eltype(x)), α)
+
+"""
+    NumOptBase.unsafe_map!(f, dst, args...)
+
+overwrites `dst` with the result of applying `f` element-wise to `args...`.
+
+This method may be extended for specific array types. By default, it uses
+loop-vectorization for strided arrays and calls `map!` for other arrays.
+
+This method is *unsafe* because it assumes without checking that `dst` and all
+`args...` have the same indices.
+
+"""
+@inline function unsafe_map!(f::Function,
+                             dst::AbstractArray,
+                             x::AbstractArray)
+    map!(f, dst, x)
+    nothing
+end
+
+@inline function unsafe_map!(f::Function,
+                             dst::StridedArray,
+                             x::StridedArray)
+    @inbounds @simd for i in eachindex(dst, x)
+        dst[i] = f(x[i])
+    end
+    nothing
+end
+
+@inline function unsafe_map!(f::Function,
+                             dst::AbstractArray,
+                             x::AbstractArray,
+                             y::AbstractArray)
+    map!(f, dst, x, y)
+    nothing
+end
+
+@inline function unsafe_map!(f::Function,
+                             dst::StridedArray,
+                             x::StridedArray,
+                             y::StridedArray)
+    @inbounds @simd for i in eachindex(dst, x, y)
+        dst[i] = f(x[i], y[i])
+    end
+    nothing
+end
+
+"""
+    NumOptBase.inner([w,] x, y)
+
+yields the inner product of `x` and `y` computed as expected by numerical
+optimization methods.
+
+"""
+function inner(x::AbstractArray{<:Number,N},
+               y::AbstractArray{<:Number,N}) where {N}
+    @assert_same_indices x y
+    return unsafe_inner(x, y)
+end
+
+function inner(w::AbstractArray{<:Real,N},
+               x::AbstractArray{<:Real,N},
+               y::AbstractArray{<:Number,N}) where {N}
+    @assert_same_indices w x y
+    return unsafe_inner(w, x, y)
+end
+
+# Inner product of variables as assumed for numerical optimization, that is
+# considering complexes as pairs of reals.
+inner(w::Real, x::Real, y::Real) = w*x*y
+inner(x::Real, y::Real) = x*y
+inner(x::Complex, y::Complex) = real(x)*real(y) + imag(x)*imag(y)
+
+function unsafe_inner(x::StridedArray,
+                      y::StridedArray)
+    acc = inner(zero(eltype(x)), zero(eltype(y)))
+    @inbounds @simd for i in eachindex(x, y)
+        acc += inner(x[i], y[i])
+    end
+    return acc
+end
+
+function unsafe_inner(x::AbstractArray,
+                      y::AbstractArray)
+    return mapreduce(inner, +, x, y)
+end
+
+function unsafe_inner(w::StridedArray,
+                      x::StridedArray,
+                      y::StridedArray)
+    acc = inner(zero(eltype(w)), zero(eltype(x)), zero(eltype(y)))
+    @inbounds @simd for i in eachindex(w, x, y)
+        acc += inner(w[i], x[i], y[i])
+    end
+    return acc
+end
+
+function unsafe_inner(w::AbstractArray,
+                      x::AbstractArray,
+                      y::AbstractArray)
+    return mapreduce(inner, +, w, x, y)
+end
+
+"""
+    NumOptBase.norm1(x)
+
+yields the ℓ₁ norm of `x` considered as a *vector* (i.e; as if `x` has been
+flattened).
+
+"""
+norm1(x::AbstractArray) = mapreduce(abs, +, x)
+function norm1(x::StridedArray)
+    acc = abs(zero(eltype(x)))
+    @inbounds @simd for i in eachindex(x)
+        acc += abs(x[i])
+    end
+    return acc
+end
+
+"""
+    NumOptBase.norm2(x)
+
+yields the Euclidean norm of `x` considered as a *vector* (i.e; as if `x` has
+been flattened).
+
+"""
+function norm2(x::StridedArray)
+    acc = abs2(zero(eltype(x)))
+    @inbounds @simd for i in eachindex(x)
+        acc += abs2(x[i])
+    end
+    return sqrt(acc)
+end
+
+norm2(x::AbstractVector) = sqrt(x'*x)
+norm2(x::AbstractArray) = norm2(flatten(x))
+
+"""
+    NumOptBase.norminf(x)
+
+yields the infinite norm of `x` considered as a *vector* (i.e; as if `x` has
+been flattened).
+
+"""
+norminf(x::AbstractArray) = reduce(max_abs, x; init = abs(zero(eltype(x))))
+function norminf(x::StridedArray)
+    acc = abs(zero(eltype(x)))
+    # FIXME: @turbo does not work here
+    @inbounds @simd for i in eachindex(x)
+        acc = max_abs(acc, x[i])
+    end
+    return acc
+end
+
+max_abs(x, y) = (abs_y = abs(y)) > x ? abs_y : x
+
+flatten(x::AbstractVector) = x
+flatten(x::AbstractArray) = reshape(x, length(x))
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,144 @@
+module TestingNumOptBase
+
 using NumOptBase
 using Test
+using Base: @propagate_inbounds
 
-@testset "NumOptBase.jl" begin
-    # Write your tests here.
+# Reference methods.
+function ref_norm1(x)
+    s = zero(eltype(x))
+    for xᵢ in x
+        s += abs(xᵢ)
+    end
+    return s
 end
+function ref_norm2(x)
+    s = zero(eltype(x))
+    for xᵢ in x
+        s += abs2(xᵢ)
+    end
+    return sqrt(s)
+end
+function ref_norminf(x)
+    s = zero(eltype(x))
+    for xᵢ in x
+        s = max(s, abs(xᵢ))
+    end
+    return s
+end
+function ref_inner(x,y)
+    s = zero(eltype(x))*zero(eltype(y))
+    for (xᵢ,yᵢ) in zip(x,y)
+        s += xᵢ*yᵢ
+    end
+    return s
+end
+function ref_inner(w,x,y)
+    s = zero(eltype(w))*zero(eltype(x))*zero(eltype(y))
+    for (wᵢ,xᵢ,yᵢ) in zip(w,x,y)
+        s += wᵢ*xᵢ*yᵢ
+    end
+    return s
+end
+
+# Custom array type to check other versions than the ones that apply for
+# strided arrays.
+struct MyArray{T,N,L,A<:AbstractArray{T,N}} <: AbstractArray{T,N}
+    parent::A
+    MyArray(arr::A) where {T,N,A<:AbstractArray{T,N}} =
+        new{T,N,IndexStyle(A)===IndexLinear(),A}(arr)
+end
+Base.parent(A::MyArray) = A.parent
+Base.length(A::MyArray) = length(A.parent)
+Base.size(A::MyArray) = size(A.parent)
+Base.axes(A::MyArray) = axes(A.parent)
+@inline Base.axes(A::MyArray) = axes(A.parent)
+@inline Base.IndexStyle(::Type{<:MyArray{T,N,false}}) where {T,N} = IndexCartesian()
+@inline function Base.getindex(A::MyArray{T,N,false}, I::Vararg{Int,N}) where {T,N}
+    @boundscheck checkbounds(A, I...)
+    return @inbounds getindex(parent(A), I...)
+end
+@inline function Base.setindex!(A::MyArray{T,N,false}, x, I::Vararg{Int,N}) where {T,N}
+    @boundscheck checkbounds(A, I...)
+    @inbounds setindex!(parent(A), x, I...)
+    return A
+end
+@inline Base.IndexStyle(::Type{<:MyArray{T,N,true}}) where {T,N} = IndexLinear()
+@inline function Base.getindex(A::MyArray{T,N,true}, i::Int) where {T,N}
+    @boundscheck checkbounds(A, i)
+    return @inbounds getindex(parent(A), i)
+end
+@inline function Base.setindex!(A::MyArray{T,N,true}, x, i::Int) where {T,N}
+    @boundscheck checkbounds(A, i)
+    @inbounds setindex!(parent(A), x, i)
+    return A
+end
+Base.similar(A::MyArray, ::Type{T}, dims::Dims) where {T} =
+    MyArray(similar(parent(A), T, dims))
+
+T′s = (Float32, Float64)
+F′s = (StridedArray, AbstractArray)
+α′s = (0, 1, -1, 2.1)
+β′s = (0, 1, -1, -1.7)
+dims = (3,4,5)
+@testset "NumOptBase T=$T, $F" for F ∈ F′s, T ∈ T′s
+    wrapper = F === StridedArray ? identity : MyArray
+    w = wrapper(rand(T, dims))
+    x = wrapper(rand(T, dims))
+    y = wrapper(rand(T, dims))
+    z = similar(x)
+    @testset "norm1" begin
+        let res = @inferred NumOptBase.norm1(x)
+            @test typeof(res) === T
+            @test res ≈ ref_norm1(x)
+        end
+    end
+    @testset "norm2" begin
+        let res = @inferred NumOptBase.norm2(x)
+            @test typeof(res) === T
+            @test res ≈ ref_norm2(x)
+        end
+    end
+    @testset "norminf" begin
+        let res = @inferred NumOptBase.norminf(x)
+            @test typeof(res) === T
+            @test res ≈ ref_norminf(x)
+        end
+    end
+    @testset "inner product" begin
+        let res = @inferred NumOptBase.inner(x, y)
+            @test typeof(res) === T
+            @test res ≈ ref_inner(x, y)
+        end
+        let res = @inferred NumOptBase.inner(w, x, y)
+            @test typeof(res) === T
+            @test res ≈ ref_inner(w, x, y)
+        end
+    end
+    @testset "scale! α = $α" for α in α′s
+        let res = @inferred NumOptBase.scale!(z, α, x)
+            @test res === z
+            @test res ≈ (@. α*x)
+        end
+    end
+    @testset "multiply!" begin
+        let res = @inferred NumOptBase.multiply!(z, x, y)
+            @test res === z
+            @test res ≈ (@. x*y)
+        end
+    end
+    @testset "update! α = $α" for α in α′s
+        let res = @inferred NumOptBase.update!(copyto!(z, y), α, x)
+            @test res === z
+            @test res ≈ (@. y + α*x)
+        end
+    end
+    @testset "combine! α = $α, β = $β" for α in α′s, β ∈ β′s
+        let res = @inferred NumOptBase.combine!(z, α, x, β, y)
+            @test res === z
+            @test res ≈ (@. α*x + β*y)
+        end
+    end
+end
+
+end # module
