a121e621bf5ec9d1dd411c9c7be669a7ec958287
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-05-26 18:11:10 +0200

    Manage to reproduce tests with LoopVectorization

--- test/runtests.jl
+++ test/runtests.jl
@@ -76,69 +76,80 @@ end
 Base.similar(A::MyArray, ::Type{T}, dims::Dims) where {T} =
     MyArray(similar(parent(A), T, dims))
 
-T′s = (Float32, Float64)
-F′s = (:StridedArray, :AbstractArray)
-α′s = (0, 1, -1, 2.1)
-β′s = (0, 1, -1, -1.7)
-dims = (3,4,5)
-@testset "NumOptBase T=$T, $F" for F ∈ F′s, T ∈ T′s
-    wrapper = F === :StridedArray ? identity : MyArray
-    w = wrapper(rand(T, dims))
-    x = wrapper(rand(T, dims))
-    y = wrapper(rand(T, dims))
-    z = similar(x)
-    @testset "norm1" begin
-        let res = @inferred NumOptBase.norm1(x)
-            @test typeof(res) === T
-            @test res ≈ ref_norm1(x)
+function runtests()
+    T′s = (Float32, Float64)
+    F′s = (:StridedArray, :AbstractArray)
+    α′s = (0, 1, -1, 2.1)
+    β′s = (0, 1, -1, -1.7)
+    dims = (3,4,5)
+    @testset "NumOptBase T=$T, $F" for F ∈ F′s, T ∈ T′s
+        wrapper = F === :StridedArray ? identity : MyArray
+        w = wrapper(rand(T, dims))
+        x = wrapper(rand(T, dims))
+        y = wrapper(rand(T, dims))
+        z = similar(x)
+        @testset "norm1" begin
+            let res = @inferred NumOptBase.norm1(x)
+                @test typeof(res) === T
+                @test res ≈ ref_norm1(x)
+            end
+        end
+        @testset "norm2" begin
+            let res = @inferred NumOptBase.norm2(x)
+                @test typeof(res) === T
+                @test res ≈ ref_norm2(x)
+            end
+        end
+        @testset "norminf" begin
+            let res = @inferred NumOptBase.norminf(x)
+                @test typeof(res) === T
+                @test res ≈ ref_norminf(x)
+            end
+        end
+        @testset "inner product" begin
+            let res = @inferred NumOptBase.inner(x, y)
+                @test typeof(res) === T
+                @test res ≈ ref_inner(x, y)
+            end
+            let res = @inferred NumOptBase.inner(w, x, y)
+                @test typeof(res) === T
+                @test res ≈ ref_inner(w, x, y)
+            end
+        end
+        @testset "scale! α = $α" for α in α′s
+            let res = @inferred NumOptBase.scale!(z, α, x)
+                @test res === z
+                @test res ≈ (@. α*x)
+            end
+        end
+        @testset "multiply!" begin
+            let res = @inferred NumOptBase.multiply!(z, x, y)
+                @test res === z
+                @test res ≈ (@. x*y)
+            end
+        end
+        @testset "update! α = $α" for α in α′s
+            let res = @inferred NumOptBase.update!(copyto!(z, y), α, x)
+                @test res === z
+                @test res ≈ (@. y + α*x)
+            end
+        end
+        @testset "combine! α = $α, β = $β" for α in α′s, β ∈ β′s
+            let res = @inferred NumOptBase.combine!(z, α, x, β, y)
+                @test res === z
+                @test res ≈ (@. α*x + β*y)
+            end
         end
     end
-    @testset "norm2" begin
-        let res = @inferred NumOptBase.norm2(x)
-            @test typeof(res) === T
-            @test res ≈ ref_norm2(x)
-        end
-    end
-    @testset "norminf" begin
-        let res = @inferred NumOptBase.norminf(x)
-            @test typeof(res) === T
-            @test res ≈ ref_norminf(x)
-        end
-    end
-    @testset "inner product" begin
-        let res = @inferred NumOptBase.inner(x, y)
-            @test typeof(res) === T
-            @test res ≈ ref_inner(x, y)
-        end
-        let res = @inferred NumOptBase.inner(w, x, y)
-            @test typeof(res) === T
-            @test res ≈ ref_inner(w, x, y)
-        end
-    end
-    @testset "scale! α = $α" for α in α′s
-        let res = @inferred NumOptBase.scale!(z, α, x)
-            @test res === z
-            @test res ≈ (@. α*x)
-        end
-    end
-    @testset "multiply!" begin
-        let res = @inferred NumOptBase.multiply!(z, x, y)
-            @test res === z
-            @test res ≈ (@. x*y)
-        end
-    end
-    @testset "update! α = $α" for α in α′s
-        let res = @inferred NumOptBase.update!(copyto!(z, y), α, x)
-            @test res === z
-            @test res ≈ (@. y + α*x)
-        end
-    end
-    @testset "combine! α = $α, β = $β" for α in α′s, β ∈ β′s
-        let res = @inferred NumOptBase.combine!(z, α, x, β, y)
-            @test res === z
-            @test res ≈ (@. α*x + β*y)
-        end
-    end
+    nothing
 end
 
 end # module
+
+println(stderr, "Testing NumOptBase package...")
+TestingNumOptBase.runtests()
+if !isdefined(Main,:LoopVectorization)
+    println(stderr, "\nTesting NumOptBase package with LoopVectorization...")
+    using LoopVectorization
+    TestingNumOptBase.runtests()
+end
