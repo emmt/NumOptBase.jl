ea2c96ce913ab1c20946e9ce8b88de7ba2da1ba0
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-06-01 14:40:14 +0200

    Use block with @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,21 @@ if !isdefined(@__MODULE__, :TestingNumOptBase)
     include("testing.jl")
 end
 
-@testset "NumOptBase package" TestingNumOptBase.runtests()
+@testset "NumOptBase package" begin
+    TestingNumOptBase.runtests()
+end
 
 if !isdefined(Main,:LoopVectorization)
     using LoopVectorization
     println()
-    @testset "NumOptBase package with LoopVectorization" TestingNumOptBase.runtests()
+    @testset "NumOptBase package with LoopVectorization" begin
+        TestingNumOptBase.runtests()
+    end
 end
 
 # To test with CUDA, do something like:
-# @testset "NumOptBase package with CUDA" TestingNumOptBase.runtests(; classes = (CuArray,))
+# @testset "NumOptBase package with CUDA" begin
+#     TestingNumOptBase.runtests(; classes = (CuArray,))
+# end
 
 nothing
